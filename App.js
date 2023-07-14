be3115a1e9f3ba8516b5fb215ebad3a713b1513d
andremugabo <andremugabo@yahoo.fr>
2023-07-14 23:57:28 +0200

    second commit

--- App.js
+++ App.js
@@ -1,20 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
+import firebase from "react-native-firebase";
 
-export default function App() {
+const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    // Check if a user is already signed in
+    const unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in
+        console.log("User is signed in:", user);
+      } else {
+        // User is signed out
+        console.log("User is signed out");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleSignUp = async () => {
+    try {
+      const response = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+      console.log("User signed up successfully!", response);
+    } catch (error) {
+      console.error("Error signing up:", error);
+    }
+  };
+
+  const handleLogin = async () => {
+    try {
+      const response = await firebase
+        .auth()
+        .signInWithEmailAndPassword(email, password);
+      console.log("User logged in successfully!", response);
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
+
+  const handleForgotPassword = async () => {
+    try {
+      await firebase.auth().sendPasswordResetEmail(email);
+      Alert.alert(
+        "Password Reset",
+        "A password reset link has been sent to your email."
+      );
+    } catch (error) {
+      console.error("Error sending password reset email:", error);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+        style={{
+          height: 40,
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 10,
+        }}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+        secureTextEntry
+        style={{
+          height: 40,
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+          marginBottom: 10,
+        }}
+      />
+      <Button title="Sign Up" onPress={handleSignUp} />
+      <Button title="Login" onPress={handleLogin} />
+      <Button title="Forgot Password" onPress={handleForgotPassword} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
